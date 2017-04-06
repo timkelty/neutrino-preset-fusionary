a3ba0b8605c8cf4ee40423dba86748fc1188d7da
Tim Kelty <tim@fusionary.com>
2017-04-06 16:13:23 -0400

    Styles

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-const ManifestPlugin = require('webpack-manifest-plugin');
 const web = require('neutrino-preset-web');
 const stylelint = require('neutrino-middleware-stylelint');
 const eslint = require('neutrino-middleware-eslint');
 const postcss = require('neutrino-middleware-postcss');
+const extractStyles = require('neutrino-middleware-extractstyles');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const cssnext = require('postcss-cssnext');
 
 module.exports = (neutrino) => {
   neutrino.use(web);
-  neutrino.use(stylelint, {
-    config: {
-      extends: 'stylelint-config-suitcss',
-    }
-  });
-  neutrino.use(eslint, {
-    baseConfig: {
-      extends: ['kswedberg'],
-    },
-  });
-
+  neutrino.use(stylelint);
+  neutrino.use(eslint);
   neutrino.use(postcss, {
     plugins: [
-      cssnext()
+      cssnext({
+        browsers: ['last 2 versions', 'ie 9']
+      })
     ]
+  });
+  neutrino.use(extractStyles, {
+    // use: ['css-loader', 'postcss-loader']
   });
 
   neutrino.config
     .plugins
       .delete('html')
+      .delete('copy')
       .end()
     .plugin('manifest')
       .use(ManifestPlugin)
-      .end();
+      .end()
+    // .plugin('extract')
+    //   .use(ExtractTextPlugin, ['[name].[contenthash].css'])
+    //   .end()
+    // .plugin('copy')
+    //   .tap((args) => {
+    //     args.options = {
+    //       ignore: ['*.js*', '*.css']
+    //     }
+    //   })
+      // .end()
+    ;
 };
